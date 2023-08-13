63c228ebbec5c072b16f9865fafc785a858244aa
Jack Ong <jqong@connect.ust.hk>
2023-08-14 05:24:09 +0800

    feat: add docker compose postgres

---
+++ src/services/debank-adapter.ts
@@ -0,0 +1,22 @@
+import { DeBank } from '@/definitions/debank';
+import axios from 'axios';
+
+export class DebankAdapater {
+  public static async getProtocolList(): Promise<DeBank.Protocol> {
+    const { data } = await axios.get<DeBank.ProtocolResponse>(
+      'https://api.debank.com/protocol/list',
+      {
+        params: {
+          start: 0,
+          limit: 10000,
+          chain_id: '',
+          pool_name: '',
+          q: '',
+          order_by: '-deposit_usd_value',
+        },
+      },
+    );
+
+    return data.data.protocols;
+  }
+}
