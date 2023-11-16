ece3113ffeafff8d38ba040e7a09b5956be960fa
jack.ong <jack.ong@okg.com>
2023-11-16 13:52:51 +0800

    chore: setup custom groups for import order

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,6 +37,7 @@ module.exports = {
           order: 'asc' /* sort in ascending order. Options: ['ignore', 'asc', 'desc'] */,
           caseInsensitive: true /* ignore case. Options: [true, false] */,
         },
+        groups: ['external', 'internal', 'builtin', 'object', 'type', 'parent', 'index', 'sibling'],
       },
     ],
   },
