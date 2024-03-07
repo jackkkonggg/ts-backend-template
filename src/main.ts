82fc2ca0a0051c15419757e91d1c91363968adc2
jack.ong <jack.ong@okg.com>
2024-03-07 09:51:31 +0800

    feat: update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import { logAll } from '@/lib/utils/log-all';
 import { Decimal } from 'decimal.js-light';
+import { logAll } from '@/lib/utils/log-all';
 
-const tick = -19477681;
-const liquidity = 3100500963381461;
+const tick = -19400218;
 
 // A fixed point .128 number has at most 128 bits after the decimal,
 // which translates to about 10**38.5 in decimal.
@@ -16,12 +15,23 @@ const Q128 = new Decimal(2).pow(128);
 const sqrt_ratio_x128 = new Decimal(
   '1.000000499999875000062499960937527343729492203613268157969894399906166456215526',
 ).pow(tick);
+const actual_sqrt_ratio = 20851524678857110339703447681380084n;
+console.log({
+  actual_sqrt_ratio,
+  sqrt_ratio_x128: sqrt_ratio_x128.toString(),
+  math: new Decimal(actual_sqrt_ratio.toString()).div(Q128).toString(),
+});
+const liquidity = new Decimal(1e18);
 
 async function main() {
   const sqrt_ratio = sqrt_ratio_x128;
   const ratio = sqrt_ratio.pow(2);
+  const x = liquidity.div(sqrt_ratio);
+  const y = liquidity.mul(sqrt_ratio);
   console.log({
     tick,
+    x: x.toString(),
+    y: y.toString(),
     sqrt_ratio: sqrt_ratio.toString(),
     ratio: ratio.toString(),
   });
