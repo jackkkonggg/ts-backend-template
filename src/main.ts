b4805bf8c73d3e6bee0b1785c6b39805d3fe8f43
jack.ong <jack.ong@okg.com>
2024-04-10 11:11:45 +0800

    chore: push updates

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { Decimal } from 'decimal.js-light';
 import { logAll } from '@/lib/utils/log-all';
 
-const tick = -5;
-
 // A fixed point .128 number has at most 128 bits after the decimal,
 // which translates to about 10**38.5 in decimal.
 // That means ~78 decimals of precision should be able to represent
@@ -11,11 +9,13 @@ const tick = -5;
 // but this should be sufficient for just computing the price.
 Decimal.set({ precision: 78 });
 
+const tick = 812;
 const Q128 = new Decimal(2).pow(128);
+const Q32 = new Decimal(2).pow(32);
 const sqrt_ratio_x128 = new Decimal('1.000000499999875000062499960937527343729492203613268157969894399906166456215526')
   .pow(tick)
   .mul(Q128);
-const actual_sqrt_ratio = 340281684740682027917806508023772674664n;
+const actual_sqrt_ratio = 340420665893103510219797445980775776256n;
 console.log({
   actual_sqrt_ratio,
   sqrt_ratio_x128: sqrt_ratio_x128.toString(),
@@ -25,6 +25,7 @@ console.log({
 const liquidity = new Decimal(61836261554);
 
 async function main() {
+  const sqrt_ratio_x96 = sqrt_ratio_x128.div(Q32);
   const sqrt_ratio = sqrt_ratio_x128.div(Q128);
   const ratio = sqrt_ratio.pow(2);
   const x = liquidity.div(sqrt_ratio);
@@ -35,6 +36,7 @@ async function main() {
     y: y.toString(),
     sqrt_ratio: sqrt_ratio.toString(),
     ratio: ratio.toString(),
+    sqrt_ratio_x96: sqrt_ratio_x96.toDecimalPlaces(0).toString(),
   });
 }
 
